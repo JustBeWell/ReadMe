d1eba38408914cf6f667b92c2a0bd79ed22bb8f7
Angel Escaño <byanicoyt@gmail.com>
2025-02-25 11:59:09 +0100

    m2

--- .github/workflows/src/test/java/app/HelloTest.java
+++ .github/workflows/src/test/java/app/HelloTest.java
@@ -1,8 +1,6 @@
 package app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
